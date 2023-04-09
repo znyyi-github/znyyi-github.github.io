373df2083774de405905e34db933ca0caea92a7c
张苧尹 <zny980416540@163.com>
2023-04-09 13:51:13 +0800

    修改 a little

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from "./components/HelloWorld.vue";
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue Github" />
+  <HelloWorld msg="Vite + Vue znyyi-Github" />
 </template>
 
 <style scoped>
@@ -21,9 +21,11 @@ import HelloWorld from "./components/HelloWorld.vue";
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
